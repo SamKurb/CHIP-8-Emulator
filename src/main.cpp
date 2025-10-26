ef703b3ab62cd7d811a09396e4515d93b1ba9f9d
SamKurb <zcabsku@ucl.ac.uk>
2025-10-26 13:50:38 +0000

    Change return error code to be platform independent

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@ int main([[maybe_unused]] int argc,[[maybe_unused]] char* args[])
     {
         Emulator emulator{};
         emulator.run();
-        return 0;
+        return EXIT_SUCCESS;
     }
     catch (const std::runtime_error& exception)
     {
         std::cerr << "FATAL ERROR. Execution halted: " << exception.what() << std::endl;
+        return EXIT_FAILURE;
     }
 }
 
